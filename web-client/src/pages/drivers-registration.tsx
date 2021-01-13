9b47579ccba15e8448cd43cbbf50d2850d612207
Chris Cottier <cgcottier@gmail.com>
2021-01-13 16:56:37 -0600

    added commentary for page/components

--- web-client/src/pages/drivers-registration.tsx
+++ web-client/src/pages/drivers-registration.tsx
@@ -6,6 +6,10 @@ import React, {useState} from 'react';
 import DriversRegistrationCurrentSlide from '../shared/components/drivers-registration/CurrentSlide';
 
 const DriverRegistration: NextPage = () => {
+    //The aim of this page is to facilitate a multipage "form"
+    //For the navigation within registration, the "current slide" is saved in state
+    //The next and previous buttons component will increment and decrement the current slide
+    //Whether the user can navigate, next, previous, or submit, is determined by "current slide"
     const [currentSlide,setCurrentSlide] = useState(1);
     const numberOfSlides = 5;
 
@@ -27,7 +31,7 @@ const DriverRegistration: NextPage = () => {
             </Head>
             <Container>
                 <DriversRegistrationCurrentSlide currentSlide={currentSlide} />
-                {/* buttons below switch, currently on buttons */}
+                {/* Navigation buttons component: conditional Previous/Next/Submit buttons*/}
             </Container>
         </>
     )
