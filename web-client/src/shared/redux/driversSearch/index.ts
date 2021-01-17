61356f8500e382aa2dddd65b605d6f102af91dca
wenjiechen <wendychen9395@gmail.com>
2021-01-17 14:40:08 -0500

    create type Driver and DriversSearch & create a mock driver data under the redux/driversSearch file

--- web-client/src/shared/redux/driversSearch/index.ts
+++ web-client/src/shared/redux/driversSearch/index.ts
@@ -1,8 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
+export interface Driver {
+  id: number;
+  name: string;
+  licenceClass: string;
+  vehicleType: string;
+  policeCheck: string;
+  drivingAbstract: string;
+  willingToLift: string;
+  packingAndSorting: string;
+  riskComfortLevel: string[];
+}
+
+export interface DriversSearch {
+  drivers: Driver[];
+}
+
+// make a mock driver data as per design
+const mockDriverData: Driver = {
+  id: 1,
+  name: 'Pam Lutz',
+  licenceClass: 'A-Z',
+  vehicleType: 'Truck',
+  policeCheck: 'Last 12 months',
+  drivingAbstract: 'Last 12 months',
+  willingToLift: 'Up to 50lbs',
+  packingAndSorting: 'Yes',
+  riskComfortLevel: ['Contactless Deliveries', 'Low Risk', 'High RIsk'],
+}
+
+const initialState: DriversSearch = {
+  drivers: [mockDriverData],
+}
 
 const driversSearchSlice = createSlice({
   name: 'driversSearch',
-  initialState: {},
+  initialState,
   reducers: {
     increment: state => state,
     decrement: state => state
