f879ebc8c7d248e4904d9204d1ee7162937a8d02
OksanaSam <60159629+OksanaSam@users.noreply.github.com>
2021-01-17 20:51:18 -0500

    #6 - add test coverage (#25)
    * add test coverage
    
    * change jest config to turn off default check for coverage
    
    * no warnings
    
    Co-authored-by: Leigh Silverstein <l2silver@ryerson.ca>

--- web-client/jest.config.js
+++ web-client/jest.config.js
@@ -1,4 +1,24 @@
+const COVERAGE = 80; // test coverage threshold set to 80% 
+
 module.exports = {
+  collectCoverage: false,
+  collectCoverageFrom: [
+    'src/**/*.{js,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+    '!**/_tests_/**',
+    '!jest.config.js',
+    '!src/**/*.test.{js,ts,tsx}',
+    '!src/**/*.mock.{js,ts,tsx}'
+  ],
+  coverageThreshold: {
+    global: {
+      statements: COVERAGE,
+      branches: COVERAGE,
+      functions: COVERAGE,
+      lines: COVERAGE
+    }
+  },
   roots: ['<rootDir>'],
   moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
   moduleDirectories: ['node_modules', 'src'],
