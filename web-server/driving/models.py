fe096de4dcdbaeddf7ee3fff2f4f305cbf1df382
hnish <hnishat@hotmail.com>
2021-01-22 19:49:51 -0500

    updated driver models and migrations

--- web-server/driving/models.py
+++ web-server/driving/models.py
@@ -7,3 +7,51 @@ class ExampleDriver(models.Model):
 
     class Meta:
         ordering = ["created"]
+
+
+class Driver(models.Model):
+    DRIVER_STATUS = (
+        ("NM", "Not Matched"),
+        ("R", "Referred"),
+        ("MNC", "Matched not Confirmed"),
+        ("MAC", "Matched and Confirmed"),
+        ("FU", "Follow Up"),
+        ("MI", "Match Incomplete"),
+        ("RMR", "Rematch Required"),
+    )
+    COMMUNITIES = (
+        ("DT", "Downtown Toronto"),
+        ("NYE", "North York East"),
+        ("NYW", "North York West"),
+        ("MTT", "Mid Town Toronto"),
+        ("EYB", "East York/ Beaches"),
+        ("SE", "Scarborough East"),
+        ("SW", "Scarborough West"),
+        ("NE", "North Etobicoke"),
+        ("SE", "South Etobicoke"),
+        ("TW", "Toronto West"),
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.TextField()
+    status = models.CharField(max_length=3, choices=DRIVER_STATUS)
+    email = models.TextField()
+    advisor_name = models.TextField()
+    languages = models.TextField()
+    organization_referral = models.TextField()
+    second_match = models.TextField()
+    community = models.CharField(max_length=3, choices=COMMUNITIES)
+    other = models.TextField()
+    # availability
+    contactless = models.BooleanField()
+    high_risk_contact = models.BooleanField()
+    up_to_thirty = models.BooleanField()
+    up_to_fifty = models.BooleanField()
+    packaging_sorting = models.BooleanField()
+    # policecheck/willing
+    # driversabstract/willing
+    car_access = models.BooleanField()
+    van_truck_access = models.BooleanField()
+    one_mill_insurance = models.BooleanField()
+    two_mill_insurance = models.BooleanField()
+    G_license = models.BooleanField()
+    other_license_class = models.TextField()
