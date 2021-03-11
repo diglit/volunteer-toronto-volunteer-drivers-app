a63689d7af5227b0572daece62b52928b51a8692
Haris Ahmad <40364018+Haris188@users.noreply.github.com>
2021-03-11 08:22:22 -0500

    #55 - cleaned some code (#56)

--- web-client/src/shared/components/drivers-search/List/DriverCard/index.tsx
+++ web-client/src/shared/components/drivers-search/List/DriverCard/index.tsx
@@ -8,23 +8,70 @@ type DriverCardProps = {
 };
 
 export const DriverCard = ({ driverInfo }: DriverCardProps): JSX.Element => {
+
+  const riskComforlLevelSpanList 
+    = driverInfo
+    .riskComfortLevel
+    .map(item => (
+      <span key={item} role="riskComfortItem">
+        {item}, 
+      </span>
+    ))
+  
   return (
     <Card>
       <CardContent>
-        <Typography variant="h5"><span role="driverName">{driverInfo.name}</span></Typography>
-        <Typography>Licence Class: <span role="licenceClass">{driverInfo.licenceClass}</span></Typography>
-        <Typography>Vehicle Type: <span role="vehicleType">{driverInfo.vehicleType}</span></Typography>
-        <Typography>Police Check: <span role="policeCheck">{driverInfo.policeCheck}</span></Typography>
-        <Typography>Driving Abstract: <span role="drivingAbstract">{driverInfo.drivingAbstract}</span></Typography>
-        <Typography>Willing to Lift: <span role="willingToLift">{driverInfo.willingToLift}</span></Typography>
-        <Typography> 
-          Packing and Sorting: <span role="packingAndSorting">{driverInfo.packingAndSorting}</span>
+        <Typography variant="h5">
+          <span role="driverName">
+            {driverInfo.name}
+          </span>
         </Typography>
+
+        <Typography>
+          Licence Class: 
+          <span role="licenceClass">
+            {driverInfo.licenceClass}
+          </span>
+        </Typography>
+
+        <Typography>
+          Vehicle Type: 
+          <span role="vehicleType">
+            {driverInfo.vehicleType}
+          </span>
+        </Typography>
+
+        <Typography>
+          Police Check: 
+          <span role="policeCheck">
+            {driverInfo.policeCheck}
+          </span>
+        </Typography>
+
+        <Typography>
+          Driving Abstract: 
+          <span role="drivingAbstract">
+            {driverInfo.drivingAbstract}
+          </span>
+        </Typography>
+
+        <Typography>
+          Willing to Lift: 
+          <span role="willingToLift">
+            {driverInfo.willingToLift}
+          </span>
+        </Typography>
+
+        <Typography> 
+          Packing and Sorting: 
+          <span role="packingAndSorting">
+            {driverInfo.packingAndSorting}
+          </span>
+        </Typography>
+
         <Typography>
           Risk Comfort Level:{' '}
-          {driverInfo.riskComfortLevel.map((item) => (
-            <span key={item} role="riskComfortItem">{item}, </span>
-          ))}
+          {riskComforlLevelSpanList}
         </Typography>
       </CardContent>
     </Card>
