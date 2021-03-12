af3bcd6e4255c252d9ec47bf720bfabbf0ab8c00
wenjiechen <wendychen9395@gmail.com>
2021-03-11 19:22:42 -0500

    #47 - create mock login api

---
+++ web-client/src/pages/api/sessions/create.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+interface User {
+  email: string;
+  password: string;
+}
+
+// the user here is used for testing if the api works
+const users: User[] = [{
+  email: 'success@diglit.ca',
+  password: 'Passw0rd!'
+}];
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+
+    const { email, password } = req.body;
+
+    try {
+      // Find the user in the user list
+      const user = users.find(user => user.email === email);
+      if (!user) return res.status(401).json({ message: "Invalid email!"});
+
+      // Check if the password is correct
+      const isValid = user.password === password;
+      if (!isValid) return res.status(401).json({ message: "Invalid password!" });
+
+      // Successfully log in
+      res.json({ message: 'You have been succesfully logged in' });
+    }
+    catch(error) {
+      res.status(404).json({message: error.message})
+    }
+  }
+};
+export default handler;
