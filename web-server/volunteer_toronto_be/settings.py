5e01247354e354f586e9cc5a4e7e64ee75251398
Erik Derohanian <erik@erik-j.de>
2021-03-14 13:57:19 -0400

    switch to postgres , make sure the docker-compose is correctly wired up. closes #59

--- web-server/volunteer_toronto_be/settings.py
+++ web-server/volunteer_toronto_be/settings.py
@@ -86,11 +86,22 @@ WSGI_APPLICATION = "volunteer_toronto_be.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-    "development": dj_database_url.config(conn_max_age=600),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+    }
 }
+
+#  DATABASES = {
+#      "default": {
+#          "ENGINE": "django.db.backends.sqlite3",
+#          "NAME": BASE_DIR / "db.sqlite3",
+#      },
+#      "development": dj_database_url.config(conn_max_age=600),
+#  }
 
 
 # Password validation
