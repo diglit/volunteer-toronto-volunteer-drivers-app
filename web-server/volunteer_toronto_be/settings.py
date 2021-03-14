10efcac627a38e4b1d50ef094999af42407d6072
Erik Derohanian <erik@erik-j.de>
2021-03-14 14:04:01 -0400

    remove some commented out DB code

--- web-server/volunteer_toronto_be/settings.py
+++ web-server/volunteer_toronto_be/settings.py
@@ -95,14 +95,6 @@ DATABASES = {
     }
 }
 
-#  DATABASES = {
-#      "default": {
-#          "ENGINE": "django.db.backends.sqlite3",
-#          "NAME": BASE_DIR / "db.sqlite3",
-#      },
-#      "development": dj_database_url.config(conn_max_age=600),
-#  }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
