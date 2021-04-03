2d466a79bf6ef87dc934040b1314bea21109223c
Haris <harisahmad.188@gmail.com>
2021-04-03 16:08:10 -0400

    Button Group added

---
+++ web-client/src/shared/components/volunteerTorontoMembers/MemberTable/ButtonGroup.tsx
@@ -0,0 +1,41 @@
+
+import React from 'react'
+import { MembersListItem, deleteUser } from '../../../redux/volunteerTorontoMembers'
+import {Button, Link} from '@material-ui/core'
+import {useDispatch} from 'react-redux'
+
+interface ButtonGroupPropTypes {
+    member: MembersListItem
+}
+
+const ButtonGroup: React.FunctionComponent<ButtonGroupPropTypes> = (props:ButtonGroupPropTypes)=>{
+    const dispatch = useDispatch()
+
+    const sendResetPassRequest = ()=>{
+        // implement
+    }
+
+    const sendDeleteUserRequest = ()=>{
+        dispatch(deleteUser(props.member.id))
+    }
+
+    const navigateToEmailCompose = ()=>{
+        // implement
+    }
+    
+    return (
+        <div>
+            <Button onClick={sendResetPassRequest}>
+                Reset Password
+            </Button>
+            <Button color="secondary" onClick={sendDeleteUserRequest}>
+                Delete
+            </Button>
+            <Button color="primary" onClick={navigateToEmailCompose}>
+                <Link href="mailto:example@email.com">Email</Link>
+            </Button>
+        </div>
+    )
+}
+
+export default ButtonGroup
