bd8f594255a86b89c5328276ffab4413112b51a0
wenjiechen <wendychen9395@gmail.com>
2021-04-03 11:19:16 -0400

    WIP-#93 skeleton structure for driver search

--- web-client/src/shared/components/drivers-search/List/DriverCard/index.tsx
+++ web-client/src/shared/components/drivers-search/List/DriverCard/index.tsx
@@ -7,6 +7,9 @@ type DriverCardProps = {
   driverInfo: Driver;
 };
 
+// TODO: update the card according to:
+// https://xd.adobe.com/view/4a36317f-0fd9-444a-b1cd-e135d91ce2e0-1f65/screen/4034537d-d116-4726-8539-9f7f2d42d3f4?fullscreen&hints=off
+
 export const DriverCard = ({ driverInfo }: DriverCardProps): JSX.Element => {
 
   const riskComforlLevelSpanList 
