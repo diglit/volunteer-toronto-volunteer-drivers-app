80a1031af37bb12f9e0246e721048f0979ed2659
wenjiechen <wendychen9395@gmail.com>
2021-04-03 14:17:37 -0400

    Clean up codes in DriverFullProfile component

--- web-client/src/shared/components/drivers-search/List/DriverFullProfile/index.tsx
+++ web-client/src/shared/components/drivers-search/List/DriverFullProfile/index.tsx
@@ -29,66 +29,8 @@ const DriverFullProfile = ({driverInfo}: DriverInfoProps): JSX.Element => {
   return (
     <div>
       Driver Full Profile
-      <Card>
-      <CardContent>
-        <Typography variant="h5">
-          <span role="driverName">
-            {driverInfo.name}
-          </span>
-        </Typography>
-
-        <Typography>
-          Licence Class: 
-          <span role="licenceClass">
-            {driverInfo.licenceClass}
-          </span>
-        </Typography>
-
-        <Typography>
-          Vehicle Type: 
-          <span role="vehicleType">
-            {driverInfo.vehicleType}
-          </span>
-        </Typography>
-
-        <Typography>
-          Police Check: 
-          <span role="policeCheck">
-            {driverInfo.policeCheck}
-          </span>
-        </Typography>
-
-        <Typography>
-          Driving Abstract: 
-          <span role="drivingAbstract">
-            {driverInfo.drivingAbstract}
-          </span>
-        </Typography>
-
-        <Typography>
-          Willing to Lift: 
-          <span role="willingToLift">
-            {driverInfo.willingToLift}
-          </span>
-        </Typography>
-
-        <Typography> 
-          Packing and Sorting: 
-          <span role="packingAndSorting">
-            {driverInfo.packingAndSorting}
-          </span>
-        </Typography>
-
-        <Typography>
-          Risk Comfort Level:{' '}
-          {riskComforlLevelSpanList}
-        </Typography>
-
-        {sendRequest && <RequestPage />}
-
-        <Button onClick={sendRequestHandle}>Send Request</Button>
-      </CardContent>
-    </Card>
+      {sendRequest && <RequestPage />}
+      <Button onClick={sendRequestHandle}>Send Request</Button>
     </div>
   );
 }
