b5cad9b55c631ee3c92fb404ee32af99b36c7271
Haris Ahmad <40364018+Haris188@users.noreply.github.com>
2021-04-05 21:10:16 -0400

    Feature/104 (#111)
    * Search & table are done
    
    * Button Group added
    
    * feature complete
    
    * fixed directory structure

---
+++ web-client/src/shared/components/volunteer-toronto/members/MemberTable/index.tsx
@@ -0,0 +1,59 @@
+
+import React, { useEffect } from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import ButtonGroup from './ButtonGroup'
+import {
+    filteredMemberListSelector, 
+    loadingSelector,
+    fetchMembers
+} from '../../../../redux/volunnteer-toronto/members'
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+    Box,
+    CircularProgress,
+    Typography
+} from '@material-ui/core'
+
+const MemberTable:React.FunctionComponent = ()=>{
+    const dispatch = useDispatch()
+    const members = useSelector(filteredMemberListSelector)
+    const loading = useSelector(loadingSelector)
+
+    useEffect(()=>{
+        dispatch(fetchMembers())
+    },[])
+
+    return (
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell><Box fontWeight="bold">Name</Box></TableCell>
+                    <TableCell><Box fontWeight="bold">Job Title</Box></TableCell>
+                    <TableCell><Box fontWeight="bold">Actions</Box></TableCell>
+                </TableRow>
+            </TableHead>
+            {loading
+            ? <CircularProgress />
+            : <TableBody>
+                {members.length > 0 
+                ? members.map(member=>(
+                    <TableRow key={member.id}>
+                        <TableCell>{member.name}</TableCell>
+                        <TableCell>{member.jobTitle}</TableCell>
+                        <TableCell><ButtonGroup member={member}/></TableCell>
+                    </TableRow>
+                ))
+                : <Typography>No Member to show</Typography>
+                }
+            </TableBody>
+            }
+        </Table>
+    )
+}
+
+export default MemberTable
