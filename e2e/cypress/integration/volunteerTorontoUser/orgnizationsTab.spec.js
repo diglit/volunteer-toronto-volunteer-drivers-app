eb0d9b52c3c87d428e4e6d47d5b7a189551a7c95
Swati <Swat.dixit@gmail.com>
2021-04-07 23:21:45 -0400

    incorporated comments in new commit

--- e2e/cypress/integration/volunteerTorontoUser/orgnizationsTab.spec.js
+++ e2e/cypress/integration/volunteerTorontoUser/orgnizationsTab.spec.js
@@ -18,20 +18,23 @@ describe('Login with Valid VT Credentials', function() {
       })
 
       beforeEach(()=>{
-        // cy.findByLabelText(inputLabels.organizations).click()                        
-        // cy.focused().should('have.attr', 'name', 'Organizations') 
+         cy.findByLabelText(inputLabels.organizations).click()                        
+         cy.focused().should('have.attr', 'name', 'Organizations') 
       })
 
 
       const assertSearchBoxValidValueResults = (input)=>{
-        cy.findByLabelText(inputLabels.search).clear()
-        cy.type(input)
-        cy.contains(input, { timeout: 500})
+        cy.findByLabelText(inputLabels.search).clear().type(input)
+
+        const resultsTable = cy.findByRole('table')
+        const correctRow = resultsTable.findByText(input) 
+        cy.contains(correctRow)     
+        
+        
       }    
 
       const assertSearchBoxInValidValueResults = (input)=>{
-        cy.findByLabelText(inputLabels.search).clear()
-        cy.type(input)
+        cy.findByLabelText(inputLabels.search).clear().type(input)
         cy.findByLabelText().should('not.exist');
       }
     
@@ -40,64 +43,64 @@ describe('Login with Valid VT Credentials', function() {
 
       describe('search query with valid username',function(){
           it('Entering valid username in the search box should display results',function(){
-            // cy.findByLabelText(inputLabels.organizations).click()
-            // cy.focused().should('have.attr', 'name', 'Organizations') 
-            // assertSearchBoxValidValueResults('Anderson,Rose')
-            // assertSearchBoxValidValueResults('Anderson')  
-            // assertSearchBoxValidValueResults('Rose') 
+            cy.findByLabelText(inputLabels.organizations).click()
+            cy.focused().should('have.attr', 'name', 'Organizations') 
+            assertSearchBoxValidValueResults('Anderson,Rose')
+            assertSearchBoxValidValueResults('Anderson')  
+            assertSearchBoxValidValueResults('Rose') 
           })     
         
           it('Entering invalid username in the search box should not display results',function(){
-            // cy.findByLabelText(inputLabels.organizations).click()
-            // cy.focused().should('have.attr', 'name', 'Organizations') 
-            // assertSearchBoxValidValueResults('@@')
-            // assertSearchBoxValidValueResults('#Anderson#')  
+            cy.findByLabelText(inputLabels.organizations).click()
+            cy.focused().should('have.attr', 'name', 'Organizations') 
+            assertSearchBoxValidValueResults('@@')
+            assertSearchBoxValidValueResults('#Anderson#')  
           }) 
       })
         
 //Use case 2: VT User trying Search Query in Organizations tab with– Valid Job Title scenario  
       describe('Search by Job Title',function(){
           it('Entering valid job title in the search box should display results',function(){
-            // cy.findByLabelText(inputLabels.organizations).click()
-            // cy.focused().should('have.attr', 'name', 'Organizations') 
-            // assertSearchBoxValidValueResults('Recruitement')
-            // assertSearchBoxValidValueResults('Manager of Operations')  
+            cy.findByLabelText(inputLabels.organizations).click()
+            cy.focused().should('have.attr', 'name', 'Organizations') 
+            assertSearchBoxValidValueResults('Recruitement')
+            assertSearchBoxValidValueResults('Manager of Operations')  
           })  
         })
       
 //Use case 3: VT User trying Search Query in Organizations tab with - Valid Organization scenario    
       describe('Search by Organization',function(){
           it('Entering valid Organization name in the search box should display results',function(){
-            // assertSearchBoxValidValueResults('Food Bank')
-            // assertSearchBoxValidValueResults('NFS')  
+            assertSearchBoxValidValueResults('Food Bank')
+            assertSearchBoxValidValueResults('NFS')  
           })  
       })
 
 //Use case 4: VT User in Organizations tab with – Pause Account Scenario      
       describe('Pause Account',function(){
           it('VT User trying Search Query in Organizations tab – Pause Account',function(){
-            // assertSearchBoxValidValueResults('Mig,Bruce')
-            // cy.findByText(inputLabels.pauseAccount).click()
-            // cy.findByText(inputLabels.pausedAccount).should('exist')       
+            assertSearchBoxValidValueResults('Mig,Bruce')
+            cy.findByText(inputLabels.pauseAccount).click()
+            cy.findByText(inputLabels.pausedAccount).should('exist')       
           })
       })
 
 //Use case 5: VT User in Organizations tab with – Delete Account Scenario
       describe('Delete Account',function(){
           it('User should be able to delete an account ',function(){
-            // assertSearchBoxValidValueResults('Forst,Emma')
-            // cy.findByText(inputLabels.deleteAccount).click()
-            // assertSearchBoxInValidValueResults('Forst,Emma')
+            assertSearchBoxValidValueResults('Forst,Emma')
+            cy.findByText(inputLabels.deleteAccount).click()
+            assertSearchBoxInValidValueResults('Forst,Emma')
           })
       })
 
 //Use case 6: VT User in Organizations tab with – Email Scenario
       describe('Email User',function(){
           it('User should be able to email selected user',function(){
-            // assertSearchBoxValidValueResults('Mig,Bruce')
-            // cy.findByText(inputLabels.email).click()
-            // cy.findByText('Mig,Bruce').should('have.attr', 'href').and('include', 'contact')
-            //TBD           
+            assertSearchBoxValidValueResults('Mig,Bruce')
+            cy.findByText(inputLabels.email).click()
+            cy.findByText('Mig,Bruce').should('have.attr', 'href').and('include', 'contact')
+                     
           })
       })
 
