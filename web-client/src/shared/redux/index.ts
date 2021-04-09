9c07a814214bb30fc1767698456d167828ab670b
Haris <harisahmad.188@gmail.com>
2021-04-09 16:52:38 -0400

    organization members complete

--- web-client/src/shared/redux/index.ts
+++ web-client/src/shared/redux/index.ts
@@ -6,6 +6,7 @@ import driversSearchSlice, { DriversState } from './driversSearch';
 import driversRegistrationSlice from './driversRegistration';
 import driverApplicationSlice from './driverApplications';
 import volunteerTorontoMembersSlice from './volunnteer-toronto/members'
+import organizationMembersSlice from './organization/members'
 
 export type AppThunk = ThunkAction<void, DriversState, unknown, Action<string>>;
 
@@ -13,7 +14,8 @@ const reducer = combineReducers({
   driverApplication: driverApplicationSlice.reducer,
   driversSearch: driversSearchSlice.reducer,
   driversRegistration: driversRegistrationSlice.reducer,
-  volunteerTorontoMembers: volunteerTorontoMembersSlice.reducer
+  volunteerTorontoMembers: volunteerTorontoMembersSlice.reducer,
+  organizationMembers: organizationMembersSlice.reducer
 })
 
 export const store = configureStore({
