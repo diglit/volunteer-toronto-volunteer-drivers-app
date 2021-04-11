6f461c048bec2847cffb070c2725a7d17da48751
Cayman Heng <hengcayman@gmail.com>
2021-04-11 09:08:42 -0400

    created driver popup on the search page using Material UI

---
+++ web-client/src/shared/components/drivers-search/Popup/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Dialog, DialogTitle, DialogContent } from '@material-ui/core';
+
+interface PopupProps {
+    openPopup: boolean;
+    setOpenPopup: (arg0: boolean) => void;
+}
+
+export const Popup = ({ openPopup, setOpenPopup }: PopupProps): JSX.Element => {
+    return (
+        <Dialog open={openPopup}>
+            <DialogTitle>
+                <div>Placeholder for Title</div>
+            </DialogTitle>
+            <DialogContent>
+                <div>Placeholder for Body Content</div>
+                <button onClick={() => { setOpenPopup(false) }}>Close Card</button>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default Popup;
